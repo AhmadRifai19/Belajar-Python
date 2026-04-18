e9c0748196e739bf1d88bc700f94687b7acdae50
AhmadRifai19 <ahmadrifai34256@gmail.com>
2026-04-18 17:37:33 +0700

    menyelesaikan tugas struktur data

---
+++ Struktur Data/inventaris_hardware.py
@@ -0,0 +1,9 @@
+komponen_laptop = ["SSD NVMe 512GB", "RAM DDR4 8GB"]
+
+print(f"Komponen Laptop: {komponen_laptop}")
+
+komponen_laptop.append("Thermal Paste")
+
+print(f"Komponen Laptop setelah dilakukan update: {komponen_laptop}")
+
+print(f"Komponen Laptop yang sedang menjadi best seller: {komponen_laptop[1]}")
