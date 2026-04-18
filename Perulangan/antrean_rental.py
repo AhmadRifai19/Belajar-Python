6a277bf251b8b759e01c4811d8fa502cd45f5d3f
AhmadRifai19 <ahmadrifai34256@gmail.com>
2026-04-18 18:11:58 +0700

    meyelesaikan perulangan

---
+++ Perulangan/antrean_rental.py
@@ -0,0 +1,6 @@
+daftar_mobil = ["All New Avanza", "Innova Reborn", "Honda Brio"]
+
+for mobil in daftar_mobil:
+    print(f"Sedang memproses penyewaan untuk mobil: {mobil}")
+
+print("Semua antrean mobil telah selesai diproses.")
