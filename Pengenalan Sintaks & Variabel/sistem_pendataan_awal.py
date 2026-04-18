29afbb74c800e3b4388f4b88f5c48c22c52c1204
AhmadRifai19 <ahmadrifai34256@gmail.com>
2026-04-18 12:09:16 +0700

    membuat sistem sederhanan

---
+++ Pengenalan Sintaks & Variabel/sistem_pendataan_awal.py
@@ -0,0 +1,6 @@
+Nama_Pengguna = "Ahmad Rifai";
+Nama_Aplikasi = "Sistem Sewa Mobil Online";
+Tahun_Rilis = 2025;
+Status_Aktif = True;
+
+print("Selamat Datang Di Aplikasi Yang bernama", Nama_Aplikasi, "Yang dirilis Oleh", Nama_Pengguna, "pada Tahun", Tahun_Rilis, "dan masih", Status_Aktif, "Sampai saat ini")
