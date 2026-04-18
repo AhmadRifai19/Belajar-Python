0694fa9f3b62f0d9881ef295c2a1f38c7970f94e
AhmadRifai19 <ahmadrifai34256@gmail.com>
2026-04-18 12:24:52 +0700

    penyempurnaan kode

--- Pengenalan Sintaks & Variabel/sistem_pendataan_awal.py
+++ Pengenalan Sintaks & Variabel/sistem_pendataan_awal.py
@@ -4,8 +4,8 @@ tahun_rilis = 2025
 status_aktif = True
 
 if status_aktif == True:
-    status_aktif = "Aktif"
-else:
-    status_aktif = "Tidak Aktif"
+    text_status = "Aktif"
+else:   
+    text_status = "Tidak Aktif"
 
-print("Selamat Datang Di Aplikasi Yang bernama", nama_aplikasi, "Yang dirilis Oleh", nama_pengguna, "pada Tahun", tahun_rilis, "dan masih", status_aktif, "Sampai saat ini")
+print("Selamat Datang Di Aplikasi Yang bernama", nama_aplikasi, "Yang dirilis Oleh", nama_pengguna, "pada Tahun", tahun_rilis, "dan masih", text_status, "Sampai saat ini")
